1b9b4d006a4d975e7b30ab12a937fd2b383e6483
C. T <C. T>
2017-08-05 14:36:15 +0200

    First commit for the deployment artifacts. Only kubernetes currently supported.

---
+++ deployment/docker/build.sh
@@ -0,0 +1,24 @@
+export DOCKER_TAG=v0.2.2
+export JAR_VERSION=0.2.0-SNAPSHOT
+
+# Setting dockers environment to be that of the minikube VM. So docker images can be created/pushed easily to the kubernetes environment.
+eval $(minikube docker-env)
+
+# Build identitystorage docker image
+cp ./../../../identitystorage/target/*.jar ./identitystorage/
+cd identitystorage
+envsubst < "Dockerfile.template" > "Dockerfile"
+docker build -t blockid-identitystorage . 
+docker tag blockid-identitystorage blockid-identitystorage:$DOCKER_TAG
+rm Dockerfile
+rm *.jar
+cd ..
+
+# Build identitystorage docker image
+cp ./../../../identitymanager/target/*.jar ./identitymanager/
+cd identitymanager
+envsubst < "Dockerfile.template" > "Dockerfile"
+docker build -t blockid-identitymanager . 
+docker tag blockid-identitymanager blockid-identitymanager:$DOCKER_TAG
+rm Dockerfile
+rm *.jar
